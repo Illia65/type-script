a2d67f3c873d8b853509e3d62141662d426b4b13
Illia Ovcharuk <ilaovcaruk0@gmail.com>
2025-08-07 18:53:34 +0100

    add:  enum explanation + example

--- enum.ts
+++ enum.ts
@@ -8,8 +8,23 @@ enum TrafficLight {
 
 let ligth = TrafficLight.RED;
 if (ligth === TrafficLight.RED) {
-  console.log("Stop");
+  // console.log("Stop");
 }
 
+enum DrinkSize {
+  SMALL,
+  MEDIUM,
+  LARGE,
+}
 
+function getDrinkPrice(size: DrinkSize) {
+  if (size === DrinkSize.SMALL) return 100;
+  if (size === DrinkSize.MEDIUM) return 250;
+  if (size === DrinkSize.LARGE) return 600;
+  return 0;
+}
 
+let myDrinkSize = DrinkSize.LARGE;
+let price = getDrinkPrice(myDrinkSize);
+
+console.log(price);
